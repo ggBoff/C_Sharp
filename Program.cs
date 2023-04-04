c411bf22153752a64e09cb4ef8b9b4a2c02df761
Blgkff <222genka222@list.ru>
2023-04-04 12:58:40 +0300

    Finished tusk 4

--- Program.cs
+++ Program.cs
@@ -10,11 +10,11 @@
 // Console.WriteLine("Enter second number");
 // int b = Convert.ToInt32(Console.ReadLine());
 
-// if (a < b)
-//     Console.WriteLine("a < b");
+// if (a > b)
+//     Console.WriteLine("a > b, max is b");
 
 // else 
-//     Console.WriteLine("a>b");
+//     Console.WriteLine("a < b, max is b");
 
 
 // so it works, I checked, trust me) 
@@ -25,3 +25,38 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+// Console.WriteLine("Enter first number");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Enter second number");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Enter third  number");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// if (a > b)
+//     if (a > c)
+//     Console.WriteLine("first is max");
+//     else
+//         if (b > c)
+//             Console.WriteLine("Second is max");
+//         else 
+//             Console.WriteLine("Third is max");
+// else
+//     if (c > b)
+//         Console.WriteLine("THird is max");
+//     else 
+//         Console.WriteLine("Second is max");
+
+// And it works, I checked, trust me)
+
+//__________________________________________________________________________________________________________________________________________________________________________
+//__________________________________________________________________________________________________________________________________________________________________________
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+
