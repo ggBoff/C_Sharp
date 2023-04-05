e7a31bfe72459da15f69f6e68c556014466134fe
Blgkff <222genka222@list.ru>
2023-04-05 16:20:00 +0300

    Finished first tusk

---
+++ Sem2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Random number [10;99] , program need to show max digit from RandomNumber
+
+// int num = new Random().Next(10,100);
+// Console.WriteLine($"Was generated random number {num}");
+
+// int x1 =num / 10;
+// int x2 = num % 10;
+
+// if (x1 > x2)
+//     Console.WriteLine($"Max number is {x1}");
+// else if (x1 == x2)
+//     Console.WriteLine($"numbers are equal {x1}={x2}");
+// else
+//     Console.WriteLine($"Max number is {x2}");
+
+// // Ouytput all numbers from 1 to GeneratedRandomNum
+
+// for (int i = 1; i<=num; i++)
+//     Console.Write(i + " ");
+
+
+// Write a 3d digit number. program is deliting 2d digit of the number
+// example 321 -> 31
+
+int num =
