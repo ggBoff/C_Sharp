ed59a3eae457611534cf59e15169a06cc63e6ad3
Blgkff <222genka222@list.ru>
2023-04-05 18:11:32 +0300

    Ended task 11 and 12 in sem2

--- Sem2/Program.cs
+++ Sem2/Program.cs
@@ -18,8 +18,48 @@
 // for (int i = 1; i<=num; i++)
 //     Console.Write(i + " ");
 
+//____________________________________________________________________
+//____________________________________________________________________
 
 // Write a 3d digit number. program is deliting 2d digit of the number
 // example 321 -> 31
+// Console.WriteLine("Enter number from 100 to 999");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num > 100 & num < 1000)
+// {
+//     int x1 = num / 100;
+//     Console.WriteLine(x1);
+    
+//     int x2 = (num % 100) / 10;
+//     Console.WriteLine(x2);
+
+//     int x3 = num % 10;
+//     Console.WriteLine(x3);
+
+//     int newNum = x1*10 + x3;
+//     Console.WriteLine(newNum);
+// }
+// else 
+//     Console.WriteLine("Pls use only number from 100 to 1000");
+
+    //________________________________________________________________________
+    //________________________________________________________________________
+
+    // 12 Program wich make can we a/b without a tail  a=9, b = 3 -> 9 is "Kratno" 3 )))
+
+Console.WriteLine("Enter numbers a and b   to check is a multiple b");
+
+Console.WriteLine("enter number a");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter number b");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int result = num1 % num2;
+
+if (result == 0)
+    Console.WriteLine($"{num1} is multiple {num2}");
+else
+    Console.WriteLine($"{num1} is not multiple {num2}, remainder is {result}");
