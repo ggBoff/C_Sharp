9eff945bcf87e1db9085d0ee555f29aaa56e17a5
Blgkff <222genka222@list.ru>
2023-04-16 21:25:14 +0300

    Added whati is Method in  lec 2

--- lec2/Program.cs
+++ lec2/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+double f(double x) // возвращаемый тип данных название(тип аргумента имя аргумента)
+{
+    double result = x * x +1;
+    return result; // тело мметода -  {функция; возвращаемое значение}
+}
+
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(f(x));
